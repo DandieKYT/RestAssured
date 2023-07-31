5c202cbab2238cc9071a7d11b5215593cce8c13d
DandieKYT <75677738+DandieKYT@users.noreply.github.com>
2023-08-01 00:28:35 +0300

    api

---
+++ src/test/java/tests/TestBase.java
@@ -0,0 +1,40 @@
+package tests;
+
+import com.codeborne.selenide.Configuration;
+import helpers.Attachment;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+
+import java.util.Map;
+
+public class TestBase {
+
+Attachment attachment = new Attachment();
+
+    @BeforeAll
+    static void setUp() {
+        Configuration.browser = "chrome";
+        Configuration.browserVersion = "100.0";
+        Configuration.remote = "https://user1:1234@" + System.getProperty("selenoid_url", "selenoid.autotests.cloud/wd/hub");
+        Configuration.browserSize = "1920x1080";
+        Configuration.pageLoadStrategy = "eager";
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of("enableVNC", true, "enableVideo", true
+
+        ));
+
+        Configuration.browserCapabilities = capabilities;
+    }
+
+    @AfterEach
+    void attachments() {
+        attachment.browserLogs();
+        attachment.attachScreenshot();
+        attachment.pageSource();
+        attachment.addVideo();
+    }
+}
+
