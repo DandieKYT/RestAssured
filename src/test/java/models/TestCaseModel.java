6c18afb1f4f8e01f8ed6e6f9633b8678ecab0ea1
DandieKYT <75677738+DandieKYT@users.noreply.github.com>
2023-08-08 22:33:13 +0300

    api

--- src/test/java/models/TestCaseModel.java
+++ src/test/java/models/TestCaseModel.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 
 @Data
-@JsonIgnoreProperties(ignoreUnknown = true)
 public class TestCaseModel {
     String precondition, name, expectedResult, id;
 }
--- src/test/java/tests/AllureTest.java
+++ src/test/java/tests/AllureTest.java
@@ -12,51 +12,50 @@ import static com.codeborne.selenide.Selenide.open;
 import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 import static io.qameta.allure.Allure.step;
 import static io.restassured.RestAssured.given;
+import static java.lang.String.format;
 import static org.hamcrest.Matchers.is;
-
 
 
 public class AllureTest extends TestBase {
     String projectId = "3488";
 
     @Test
-    public void testCase(){
+    public void testCase() {
         Faker faker = new Faker();
         String someName = faker.name().fullName();
         step("Авторизация");
 
-            testCase.setName(someName);
-            CreateTestCaseResponse createTestCaseResponse = step("Create testcase", () ->
-                    given()
-                    .log().all()
-                    .header("X-XSRF-TOKEN", "38dd8d06-c8a1-45ea-af9a-7eba2dd09077")
-                    .cookies("XSRF-TOKEN", "38dd8d06-c8a1-45ea-af9a-7eba2dd09077",
-                            "ALLURE_TESTOPS_SESSION", "5cb11656-c10d-4ee4-b142-5b201dee9f90")
-                    .contentType("application/json;charset=UTF-8")
-                    .body(testCase)
-                            .queryParam("project Id", projectId)
-                    .when()
-                    .post("https://allure.autotests.cloud/api/rs/testcasetree/leaf?projectId=3488&treeId=&")
-                    .then()
-                    .log().status()
-                    .log().body()
-                    .statusCode(200)
-                    .extract().as(CreateTestCaseResponse.class));
-
-
+        testCase.setName(someName);
+        CreateTestCaseResponse createTestCaseResponse = step("Create testcase", () ->
+                given()
+                        .log().all()
+                        .header("X-XSRF-TOKEN", "38dd8d06-c8a1-45ea-af9a-7eba2dd09077")
+                        .cookies("XSRF-TOKEN", "38dd8d06-c8a1-45ea-af9a-7eba2dd09077",
+                                "ALLURE_TESTOPS_SESSION", "5cb11656-c10d-4ee4-b142-5b201dee9f90")
+                        .contentType("application/json;charset=UTF-8")
+                        .body(testCase)
+                        .queryParam("project Id", projectId)
+                        .when()
+                        .post("https://allure.autotests.cloud/api/rs/testcasetree/leaf?projectId=3488&treeId=&")
+                        .then()
+                        .log().status()
+                        .log().body()
+                        .statusCode(200)
+                        .extract().as(CreateTestCaseResponse.class));
 
 
         step("PickUp id Test case", () -> {
             open("/favicon.ico");
-            Cookie authCookie =new Cookie ("ALLURE_TESTOPS_SESSION", "5cb11656-c10d-4ee4-b142-5b201dee9f90");
+            Cookie authCookie = new Cookie("ALLURE_TESTOPS_SESSION", "5cb11656-c10d-4ee4-b142-5b201dee9f90");
             getWebDriver().manage().addCookie(authCookie);
 
             String testCaseID = createTestCaseResponse.getId();
-            String testCaseURl =String.format("/project/%s/test-cases/%s", projectId , testCaseID);
+            String testCaseURl = format("/project/%s/test-cases/%s", projectId, testCaseID);
             open(testCaseURl);
 
         });
         step("Edit testcase preconditions", () -> {
+            String testCaseID = createTestCaseResponse.getId();
             testCaseModel.setPrecondition(someName);
             given()
                     .log().all()
@@ -66,7 +65,7 @@ public class AllureTest extends TestBase {
                     .contentType("application/json;charset=UTF-8")
                     .body(testCaseModel)
                     .when()
-                    .patch("https://allure.autotests.cloud/api/rs/testcase/%s");
+                    .patch(format("/api/rs/testcase/%s", testCaseID));
 
         });
         step("Edit testcase steps", () -> {
@@ -80,7 +79,7 @@ public class AllureTest extends TestBase {
                     .contentType("application/json;charset=UTF-8")
                     .body(testCaseModel)
                     .when()
-                    .post("https://allure.autotests.cloud/api/rs/testcase/%s ");
+                    .post("https://allure.autotests.cloud/api/rs/testcase/%s");
         });
         step("Edit testcase expectedResult", () -> {
 
@@ -93,9 +92,9 @@ public class AllureTest extends TestBase {
                     .contentType("application/json;charset=UTF-8")
                     .body(testCaseModel)
                     .when()
-                    .patch("https://allure.autotests.cloud/api/rs/testcase/%s ");
+                    .patch("https://allure.autotests.cloud/api/rs/testcase/%s");
         });
 
 
-}
+    }
 }
