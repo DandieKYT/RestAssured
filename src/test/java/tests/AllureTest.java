b68a960caa8d174f1d258952e3197de89f680e9f
DandieKYT <75677738+DandieKYT@users.noreply.github.com>
2023-08-11 21:26:54 +0300

    1234

--- src/test/java/tests/AllureTest.java
+++ src/test/java/tests/AllureTest.java
@@ -1,105 +1,65 @@
 package tests;
 
 
-import com.github.javafaker.Faker;
-
 import models.CreateTestCaseResponse;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.Cookie;
 
-
-import static com.codeborne.selenide.Selenide.open;
-import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
-import static io.qameta.allure.Allure.step;
 import static io.restassured.RestAssured.given;
 import static java.lang.String.format;
 
 
 public class AllureTest extends TestBase {
-    String projectId = "3488";
 
     @Test
-    public void testCase() {
-        Faker faker = new Faker();
-        String someName = faker.name().fullName();
-        step("Авторизация");
-
-        testCase.setName(someName);
-        CreateTestCaseResponse createTestCaseResponse = step("Create testcase", () ->
-                given()
-                        .log().all()
-                        .header("X-XSRF-TOKEN", "38dd8d06-c8a1-45ea-af9a-7eba2dd09077")
-                        .cookies("XSRF-TOKEN", "38dd8d06-c8a1-45ea-af9a-7eba2dd09077",
-                                "ALLURE_TESTOPS_SESSION", "5cb11656-c10d-4ee4-b142-5b201dee9f90")
-                        .contentType("application/json;charset=UTF-8")
-                        .body(testCase)
-                        .when()
-                        .post("https://allure.autotests.cloud/api/rs/testcasetree/leaf?projectId=3488&treeId=&")
-                        .then()
-                        .log().status()
-                        .log().body()
-                        .statusCode(200)
-                        .extract().as(CreateTestCaseResponse.class));
-
-
-        step("PickUp id Test case", () -> {
-            open("/favicon.ico");
-            Cookie authCookie = new Cookie("ALLURE_TESTOPS_SESSION", "5cb11656-c10d-4ee4-b142-5b201dee9f90");
-            getWebDriver().manage().addCookie(authCookie);
-
-            String testCaseID = createTestCaseResponse.getId();
-            String testCaseURl = format("/project/%s/test-cases/%s", projectId, testCaseID);
-            open(testCaseURl);
-
-        });
-        step("Edit testcase preconditions", () -> {
-            String testCaseID = createTestCaseResponse.getId();
-            testCase.setPrecondition(someName);
-            given()
-                    .log().all()
-                    .header("X-XSRF-TOKEN", "38dd8d06-c8a1-45ea-af9a-7eba2dd09077")
-                    .cookies("XSRF-TOKEN", "38dd8d06-c8a1-45ea-af9a-7eba2dd09077",
-                            "ALLURE_TESTOPS_SESSION", "5cb11656-c10d-4ee4-b142-5b201dee9f90")
-                    .contentType("application/json;charset=UTF-8")
-                    .body(testCase)
-                    .when()
-                    .patch(format("/api/rs/testcase/%s", testCaseID))
-                    .then()
-                    .log().body();
-        });
-        step("Edit testcase steps", () -> {
-            String testCaseID = createTestCaseResponse.getId();
-            testCase.setName(someName);
-            given()
-                    .log().all()
-                    .header("X-XSRF-TOKEN", "38dd8d06-c8a1-45ea-af9a-7eba2dd09077")
-                    .cookies("XSRF-TOKEN", "38dd8d06-c8a1-45ea-af9a-7eba2dd09077",
-                            "ALLURE_TESTOPS_SESSION", "5cb11656-c10d-4ee4-b142-5b201dee9f90")
-                    .contentType("application/json;charset=UTF-8")
-                    .body(testCase)
-                    .when()
-                    .post(format("/api/rs/testcase/%s/scenario", testCaseID))
-                    .then()
-                    .log().body();
-        });
-        step("Edit testcase expectedResult", () -> {
-            String testCaseID = createTestCaseResponse.getId();
-            testCase.setExpectedResult(someName);
-            given()
-                    .log().all()
-                    .header("X-XSRF-TOKEN", "38dd8d06-c8a1-45ea-af9a-7eba2dd09077")
-                    .cookies("XSRF-TOKEN", "38dd8d06-c8a1-45ea-af9a-7eba2dd09077",
-                            "ALLURE_TESTOPS_SESSION", "5cb11656-c10d-4ee4-b142-5b201dee9f90")
-                    .contentType("application/json;charset=UTF-8")
-                    .body(testCase)
-                    .when()
-                    .patch(format("/api/rs/testcase/%s", testCaseID))
-                    .then()
-                    .log().body();
-
-        });
-
-
+    public void testPrecondition() {
+        String testCaseID = createTestCaseResponse.getId();
+        testCase.setPrecondition("1234");
+        given()
+                .log().all()
+                .header("X-XSRF-TOKEN", "38dd8d06-c8a1-45ea-af9a-7eba2dd09077")
+                .cookies("XSRF-TOKEN", "38dd8d06-c8a1-45ea-af9a-7eba2dd09077",
+                        "ALLURE_TESTOPS_SESSION", "5cb11656-c10d-4ee4-b142-5b201dee9f90")
+                .contentType("application/json;charset=UTF-8")
+                .body(testCase)
+                .when()
+                .patch(format("/api/rs/testcase/%s", testCaseID))
+                .then()
+                .log().body();
     }
 
+
+    @Test
+    public void testSteps() {
+        String testCaseID = createTestCaseResponse.getId();
+        testCase.setName("1234");
+        given()
+                .log().all()
+                .header("X-XSRF-TOKEN", "38dd8d06-c8a1-45ea-af9a-7eba2dd09077")
+                .cookies("XSRF-TOKEN", "38dd8d06-c8a1-45ea-af9a-7eba2dd09077",
+                        "ALLURE_TESTOPS_SESSION", "5cb11656-c10d-4ee4-b142-5b201dee9f90")
+                .contentType("application/json;charset=UTF-8")
+                .body(testCase)
+                .when()
+                .post(format("/api/rs/testcase/%s/scenario", testCaseID))
+                .then()
+                .log().body();
+    }
+
+    @Test
+    public void testExpectedResult() {
+        String testCaseID = createTestCaseResponse.getId();
+        testCase.setExpectedResult("1234");
+        given()
+                .log().all()
+                .header("X-XSRF-TOKEN", "38dd8d06-c8a1-45ea-af9a-7eba2dd09077")
+                .cookies("XSRF-TOKEN", "38dd8d06-c8a1-45ea-af9a-7eba2dd09077",
+                        "ALLURE_TESTOPS_SESSION", "5cb11656-c10d-4ee4-b142-5b201dee9f90")
+                .contentType("application/json;charset=UTF-8")
+                .body(testCase)
+                .when()
+                .patch(format("/api/rs/testcase/%s", testCaseID))
+                .then()
+                .log().body();
+    }
 }
+
--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -4,15 +4,49 @@ import com.codeborne.selenide.Configuration;
 import io.restassured.RestAssured;
 import models.CreateTestCaseResponse;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.Cookie;
+
+import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
+import static io.qameta.allure.Allure.step;
+import static io.restassured.RestAssured.given;
+import static java.lang.String.format;
 
 public class TestBase {
-
+    String token = "38dd8d06-c8a1-45ea-af9a-7eba2dd09077", session = "b68e6bbe-e67c-4828-85b4-3f828faf08bd";
     CreateTestCaseResponse testCase = new CreateTestCaseResponse();
     @BeforeAll
-    static void setUp() {
+
+    public void setUp() {
+        CreateTestCaseResponse testCase = new CreateTestCaseResponse();
         Configuration.baseUrl = "https://allure.autotests.cloud";
         Configuration.holdBrowserOpen = true;
 
         RestAssured.baseURI = "https://allure.autotests.cloud";
-    }
-}
+
+        testCase.setName("1234");
+        CreateTestCaseResponse createTestCaseResponse = step("Create testcase", () ->
+                given()
+                        .log().all()
+                        .header("X-XSRF-TOKEN", token)
+                        .cookies("XSRF-TOKEN", token,
+                                "ALLURE_TESTOPS_SESSION", session)
+                        .contentType("application/json;charset=UTF-8")
+                        .body(testCase)
+                        .when()
+                        .post("https://allure.autotests.cloud/api/rs/testcasetree/leaf?projectId=3488&treeId=&")
+                        .then()
+                        .log().status()
+                        .log().body()
+                        .statusCode(200)
+                        .extract().as(CreateTestCaseResponse.class));
+
+
+        open("/favicon.ico");
+        Cookie authCookie = new Cookie("ALLURE_TESTOPS_SESSION", session);
+        getWebDriver().manage().addCookie(authCookie);
+
+        String testCaseID = createTestCaseResponse.getId();
+        String testCaseURl = format("/project/3488/test-cases/%s", testCaseID);
+        open(testCaseURl);
+    }}
